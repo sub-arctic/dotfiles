aa2ede3cb8416b65561c1ab708e014f6f04e92a0
sub-arctic <arthurhawron@duck.com>
2023-11-24 18:31:48 +1100

    updated for new distro

---
+++ fish/functions/nmcli.fish
@@ -0,0 +1,4 @@
+function nmcli --description 'alias nmcli nmcli --colors yes'
+ command nmcli --colors yes $argv
+        
+end
---
+++ fish/functions/youtube-dl.fish
@@ -0,0 +1,4 @@
+function youtube-dl --wraps=yt-dlp --description 'alias youtube-dl yt-dlp'
+  yt-dlp $argv
+        
+end
