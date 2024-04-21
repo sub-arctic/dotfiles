d82614d8b0f66dd6355fa38ca24558bdb4b871d3
sub-arctic <mail@subarctic.tech>
2024-04-21 21:59:52 +1000

    update neovim

---
+++ dot_config/nvim/lua/sub-arctic/bufferline.lua
@@ -0,0 +1,11 @@
+vim.opt.termguicolors = true
+local bufferline = require("bufferline")
+bufferline.setup {
+    options = {
+        mode = "buffers",
+        themable = true,
+        numbers = "ordinal",
+        diagnostics = "nvim_lsp",
+        separator_style = "slope",
+    },
+}
