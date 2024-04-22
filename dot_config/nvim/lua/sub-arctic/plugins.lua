9fc3ea2336391374228353381e888abecb97cf38
sub-arctic <mail@subarctic.tech>
2024-04-22 22:06:19 +1000

    first implementations of performance enhancements

--- dot_config/nvim/lua/sub-arctic/plugins.lua
+++ dot_config/nvim/lua/sub-arctic/plugins.lua
@@ -41,6 +41,9 @@ require("lazy").setup({
     {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
+        keys = {
+            { "<leader>ft", "<cmd>Neotree toggle<cr>", desc="NeoTree"}
+        },
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
