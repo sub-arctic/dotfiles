d5b8d4f6719cffb3e90388d5145119a6c16cc995
sub-arctic <mail@subarctic.tech>
2024-04-26 23:45:30 +1000

    updated alpha.nvim
    Signed-off-by: sub-arctic <mail@subarctic.tech>

--- dot_config/nvim/lua/plugins/alpha.lua
+++ dot_config/nvim/lua/plugins/alpha.lua
@@ -1,2 +1,28 @@
 local alpha = require("alpha")
-alpha.setup(require'alpha.themes.dashboard'.config)
+local dashboard = require("alpha.themes.dashboard")
+
+dashboard.section.header.val = {
+    "                                                     ",
+    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+    "                                                     ",
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button( "e", "󰝒  > New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "f", "󰈞  > Find file", ":Telescope find_files<CR>"),
+    dashboard.button( "r", "󱔗  > Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "s", "󰑴  > Note Taking" , ":cd ~/documents/vault<CR> :Neotree left<CR> :Telescope oldfiles <CR>"),
+    dashboard.button( "q", "󰈆  > Quit NVIM", ":qa<CR>"),
+}
+
+alpha.setup(dashboard.opts)
+
+vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
