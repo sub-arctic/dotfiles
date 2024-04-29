31347ca0da989358f56c1d664017943c29bce045
sub-arctic <mail@subarctic.tech>
2024-04-29 19:38:38 +1000

    added some keymaps to nvim
    Signed-off-by: sub-arctic <mail@subarctic.tech>

--- dot_config/nvim/lua/lazy-plugins.lua
+++ dot_config/nvim/lua/lazy-plugins.lua
@@ -39,9 +39,6 @@ lazy.setup({
     {
         'nvim-neo-tree/neo-tree.nvim',
         branch = 'v3.x',
-        keys = {
-            { '<leader>ft', '<cmd>Neotree toggle<cr>', desc='NeoTree'}
-        },
         dependencies = {
             'nvim-lua/plenary.nvim',
             'nvim-tree/nvim-web-devicons',
