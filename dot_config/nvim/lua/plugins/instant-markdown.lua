c60ee943978800b2cbb9c2aa63a70f25912bad2e
sub-arctic <mail@subarctic.tech>
2024-05-06 12:43:27 +1000

    updated instant-markdown
    Signed-off-by: sub-arctic <mail@subarctic.tech>

--- dot_config/nvim/lua/plugins/instant-markdown.lua
+++ dot_config/nvim/lua/plugins/instant-markdown.lua
@@ -1,12 +1,7 @@
-vim.g.instant_markdown_slow = 1
-vim.g.instant_markdown_autostart = 1
-vim.g.instant_markdown_open_to_the_world = 0
 vim.g.instant_markdown_allow_unsafe_content = 1
 vim.g.instant_markdown_allow_external_content = 1
 vim.g.instant_markdown_mathjax = 1
 vim.g.instant_markdown_mermaid = 1
 vim.g.instant_markdown_logfile = '/tmp/instant_markdown.log'
-vim.g.instant_markdown_autoscroll = 0
 vim.g.instant_markdown_port = 8090
-vim.g.instant_markdown_python = 1
 vim.g.instant_markdown_theme = 'light'
