ddcd05296800b236ec1500a256447cb9c0347e5f
sub-arctic <mail@subarctic.tech>
2024-05-10 09:13:04 +1000

    added cmp-path
    Signed-off-by: sub-arctic <mail@subarctic.tech>

--- dot_config/nvim/lua/lazy-plugins.lua
+++ dot_config/nvim/lua/lazy-plugins.lua
@@ -7,6 +7,7 @@ lazy.setup({
     'ahmedkhalf/project.nvim',
     'lewis6991/gitsigns.nvim',
     'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-path',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-cmdline',
     { 'hrsh7th/nvim-cmp', dependencies = "onsails/lspkind.nvim" },
