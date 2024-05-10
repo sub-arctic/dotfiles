5e74411c926ea41e9782833b8e5193f7823f897f
sub-arctic <mail@subarctic.tech>
2024-05-11 01:10:21 +1000

    added back in cmdline cmp

--- dot_config/nvim/lua/lazy-plugins.lua
+++ dot_config/nvim/lua/lazy-plugins.lua
@@ -10,10 +10,12 @@ lazy.setup({
 	'hrsh7th/nvim-cmp',
 	dependencies = {
 		'onsails/lspkind.nvim',
+		'f3fora/cmp-spell',
 		'kdheepak/cmp-latex-symbols',
 		'hrsh7th/cmp-nvim-lsp',
 		'hrsh7th/cmp-path',
 		'hrsh7th/cmp-buffer',
+		'hrsh7th/cmp-cmdline',
 		'hrsh7th/cmp-vsnip',
 		'hrsh7th/vim-vsnip',
 		'hrsh7th/vim-vsnip-integ',
