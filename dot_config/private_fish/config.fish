9694e54db52612fecf0b8f7bb7abe70cbcc7cf04
sub-arctic <mail@subarctic.tech>
2024-05-27 21:25:01 +1000

    added colorpick

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,6 @@
 function add-ssh-key
     if not set -q SSH_AGENT_PID
-        eval (ssh-agent -c -t 3600 > /dev/null)
+        eval (ssh-agent -c -t 3600)
         set -l SSH_AGENT_PID $last_pid
         ssh-add ~/.ssh/id_ed25519
     else
