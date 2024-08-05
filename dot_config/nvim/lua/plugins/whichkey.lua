f085ef8dbc260024f08fed59dc3d3e0db6775faa
sub-arctic <mail@subarctic.tech>
2024-08-05 17:24:58 +1000

    fixed depreciated configurations in which-key (neovim plugin)

--- dot_config/nvim/lua/plugins/whichkey.lua
+++ dot_config/nvim/lua/plugins/whichkey.lua
@@ -11,7 +11,6 @@ whichkey.setup {
         },
         presets = {
           operators = true, -- adds help for operators like d, y, ...
-          motions = true, -- adds help for motions
           text_objects = true, -- help for text objects triggered after entering an operator
           windows = true, -- default bindings on <c-w>
           nav = true, -- misc bindings to work with windows
@@ -21,27 +20,13 @@ whichkey.setup {
     },
   -- add operators that will trigger motion and text object completion
   -- to enable all native operators, set the preset / operators plugin above
-    operators = { gc = "Comments" },
-    key_labels = {
-    -- override the label used to display some keys. It doesn't effect WK in any other way.
-    -- For example:
-    -- ["<space>"] = "SPC",
-    -- ["<cr>"] = "RET",
-    -- ["<tab>"] = "TAB",
-    },
-    motions = {
-        count = true,
-    },
+    defer = { gc = "Comments" },
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
         separator = "→", -- symbol used between a key and it's label
         group = "+", -- symbol prepended to a group
     },
-    popup_mappings = {
-        scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
-    },
-    window = {
+    win = {
         border = "none", -- none, single, double, shadow
         position = "bottom", -- bottom, top
         margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]. When between 0 and 1, will be treated as a percentage of the screen size.
@@ -55,14 +40,9 @@ whichkey.setup {
         spacing = 3, -- spacing between columns
         align = "left", -- align columns left, center or right
     },
-    ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "^:", "^ ", "^call ", "^lua " }, -- hide mapping boilerplate
     show_help = true, -- show a help message in the command line for using WhichKey
     show_keys = true, -- show the currently pressed key and its label as a message in the command line
-    triggers = "auto", -- automatically setup triggers
-    -- triggers = {"<leader>"} -- or specifiy a list manually
-    -- list of triggers, where WhichKey should not wait for timeoutlen and show immediately
-    triggers_nowait = {
+    delay = {
         -- marks
         "`",
         "'",
@@ -73,12 +53,6 @@ whichkey.setup {
         "<c-r>",
         -- spelling
         "z=",
-    },
-    triggers_blacklist = {
-        -- list of mode / prefixes that should never be hooked by WhichKey
-        -- this is mostly relevant for keymaps that start with a native binding
-        i = { "j", "k" },
-        v = { "j", "k" },
     },
     -- disable the WhichKey popup for certain buf types and file types.
     -- Disabled by default for Telescope
