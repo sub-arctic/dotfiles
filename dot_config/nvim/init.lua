e16df334fbc1064350b7d51c813e621df9bfed4b
sub-arctic <subarctic@duck.com>
2024-11-30 21:56:38 +1100

    some updates

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,6 +9,6 @@ require 'lazy-bootstrap'
 
 require 'lazy-plugins'
 require 'plugins'
-vim.cmd [[colorscheme everforest]]
+vim.cmd [[colorscheme gruvbox-material]]
 vim.g.background=dark
 -- vim: ts=2 sts=2 sw=2 et
