e77cc8f79fa3fbf9bf8150be91c68ed571a2b7af
sub-arctic <subarctic@duck.com>
2025-03-02 21:07:59 +1100

    added gruvbox nvim options

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -9,6 +9,11 @@ require 'lazy-bootstrap'
 
 require 'lazy-plugins'
 require 'plugins'
+vim.g.gruvbox_material_enable_italic=1
+vim.g.gruvbox_material_diagnostic_text_highlight=1
+vim.g.gruvbox_material_diagnostic_line_highlight=1
+vim.g.gruvbox_material_diagnostic_virtual_text='highlighted'
+vim.g.gruvbox_material_inlay_hints_background='dimmed'
 vim.cmd [[colorscheme gruvbox-material]]
 vim.g.background=dark
 -- vim: ts=2 sts=2 sw=2 et
